0d075475b300b489c12085a19e0dfbea8d9e6aaf
Maxence <maxence.fort53@gmail.com>
2020-12-01 09:59:27 +0100

    Création du Bottom Menu

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -3,6 +3,12 @@ package com.example.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,42 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Bottom Menu
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener
+            navListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(
+                @NonNull MenuItem item)
+        {
+            // By using switch we can get
+            // the selected fragment
+            // by using there id.
+            Fragment selectedFragment = null;
+            switch (item.getItemId()) {
+                case R.id.itineraire:
+                    selectedFragment = new ItineraireFragment();
+                    break;
+                case R.id.cartes:
+                    selectedFragment = new CartesFragment();
+                    break;
+                case R.id.profil:
+                    selectedFragment = new ProfilFragment();
+                    break;
+            }
+            // It will help to replace the one fragment to other.
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(
+                            R.id.fragment_container,
+                            selectedFragment)
+                    .commit();
+            return true;
+        }
+    };
 }

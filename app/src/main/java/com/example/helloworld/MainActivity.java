6f48fe945917ba2fb055968163246165f09dbed2
Amine-Chekroud <amine.chekroud@hotmail.com>
2020-12-01 11:06:20 +0100

    Page d'accueil

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.helloworld;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Paint;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.style.UnderlineSpan;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView options = (TextView) findViewById(R.id.options);
+        options.setPaintFlags(options.getPaintFlags()|Paint.UNDERLINE_TEXT_FLAG);
+
     }
 }

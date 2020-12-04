e05697fc3dae6bd4873033d4429fb71ac0628ada
ChloeLeric <chloe@leric.eu>
2020-12-04 11:32:51 +0100

    adresses favorites fonctionelles

---
+++ app/src/main/java/com/example/helloworld/Preferences.java
@@ -0,0 +1,70 @@
+package com.example.helloworld;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Preferences {
+
+    public static void setSensibility(String key, String value, Context context){
+        SharedPreferences prefs = context.getSharedPreferences("sensibility",Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString(key, value);
+        editor.apply();
+    }
+
+    public static String getSensibility(String key, Context context){
+        SharedPreferences prefs = context.getSharedPreferences("sensibility", Context.MODE_PRIVATE);
+        return prefs.getString(key, "notfound");
+    }
+
+    public static void setPrefAddresses(String arrayName, ArrayList<String> array, Context context) {
+        SharedPreferences prefs = context.getSharedPreferences("listOfAddresses", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putInt(arrayName +"_size", array.size());
+        for(int i=0;i<array.size();i++)
+            editor.putString(arrayName + "_" + i, array.get(i));
+        editor.apply();
+    }
+
+    public static ArrayList<String> getPrefAddresses(String arrayName, Context context) {
+        SharedPreferences prefs = context.getSharedPreferences("listOfAddresses", Context.MODE_PRIVATE);
+        int size = prefs.getInt(arrayName + "_size", 0);
+        ArrayList<String> array = new ArrayList<>(size);
+        for(int i=0;i<size;i++)
+            array.add(prefs.getString(arrayName + "_" + i, null));
+        return array;
+    }
+
+    public static int getNumberOfAddresses(String arrayName, Context context) {
+        SharedPreferences prefs = context.getSharedPreferences("listOfAddresses", Context.MODE_PRIVATE);
+        int numberOfAddresses = prefs.getInt(arrayName + "_size", 0);
+        return numberOfAddresses;
+    }
+
+    public static void removeValue(String arrayName, int ind, Context context){
+        SharedPreferences prefs = context.getSharedPreferences("listOfAddresses", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.remove(arrayName + "_" + ind);
+        int newSize = prefs.getInt(arrayName+"_size",0)-1;
+        editor.putInt(arrayName+"_size",newSize);
+        editor.apply();
+    }
+
+    public static void addValue(String arrayName, int key, String value, Context context){
+        SharedPreferences prefs = context.getSharedPreferences("listOfAddresses", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString(arrayName+"_"+key,value);
+        int newSize = prefs.getInt(arrayName+"_size",0)+1;
+        editor.putInt(arrayName+"_size",newSize);
+        editor.apply();
+    }
+    public static void clear(String arrayName, Context context){
+        SharedPreferences prefs = context.getSharedPreferences("listOfAddresses", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.clear();
+        editor.apply();
+    }
+}
--- app/src/main/java/com/example/helloworld/ProfileActivity.java
+++ app/src/main/java/com/example/helloworld/ProfileActivity.java
@@ -3,6 +3,7 @@ package com.example.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Bundle;
@@ -18,6 +19,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.PopupWindow;
 import android.widget.RelativeLayout;
@@ -25,6 +27,8 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import org.w3c.dom.Text;
+
+import java.util.ArrayList;
 
 public class ProfileActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -34,22 +38,28 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
 
     private View dim_popup;
 
+    private int buttonView;
+    private PopupWindow popupWindow;
+
+    // addresses
     private Button addButton;
     private EditText enterAddress;
     private String addedAddress;
     private TextView address1;
-
-    private int buttonView;
-    private PopupWindow popupWindow;
-
-    private SharedPreferences preferences;
+    private TextView address2;
+    private TextView address3;
+    private TextView address4;
+    private TextView address5;
+    private ImageButton remove1;
+    private ImageButton remove2;
+    private ImageButton remove3;
+    private ImageButton remove4;
+    private ImageButton remove5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
-
-        preferences = getPreferences(MODE_PRIVATE);
 
         //link layout elements to activity
         sensibilityButton = findViewById(R.id.sensibility);
@@ -68,9 +78,8 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
         favoriteAdressesButton.setOnClickListener(this);
         favoriteTransportationButton.setOnClickListener(this);
 
-        LayoutInflater inflater = (LayoutInflater)
-                getSystemService(LAYOUT_INFLATER_SERVICE);
-        View popupView = inflater.inflate(R.layout.popup_favorite_adresses,null);
+        LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+        final View popupView = inflater.inflate(R.layout.popup_favorite_adresses,null);
 
         // create the popup window
         int width = RelativeLayout.LayoutParams.WRAP_CONTENT;
@@ -93,10 +102,60 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
             }
         });
 
+        //link elements from popup window
         addButton = popupView.findViewById(R.id.button_add);
         enterAddress = popupView.findViewById(R.id.enter_adress);
         address1 = popupView.findViewById(R.id.adress1);
+        address2 = popupView.findViewById(R.id.adress2);
+        address3 = popupView.findViewById(R.id.adress3);
+        address4 = popupView.findViewById(R.id.adress4);
+        address5 = popupView.findViewById(R.id.adress5);
+        remove1 = popupView.findViewById(R.id.remove_btn_1);
+        remove2 = popupView.findViewById(R.id.remove_btn_2);
+        remove3 = popupView.findViewById(R.id.remove_btn_3);
+        remove4 = popupView.findViewById(R.id.remove_btn_4);
+        remove5 = popupView.findViewById(R.id.remove_btn_5);
 
+        //set tags to know which address to display
+        address1.setTag(0);
+        address2.setTag(1);
+        address3.setTag(2);
+        address4.setTag(3);
+        address5.setTag(4);
+        remove1.setTag(5);
+        remove2.setTag(6);
+        remove3.setTag(7);
+        remove4.setTag(8);
+        remove5.setTag(9);
+
+        View.OnClickListener onCLickRemove = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int i = (int) v.getTag();
+                popupView.findViewWithTag(i-5).setVisibility(View.GONE);
+                popupView.findViewWithTag(i).setVisibility(View.GONE);
+                Preferences.removeValue("Address",i-5,ProfileActivity.this);
+            }
+        };
+
+        remove1.setOnClickListener(onCLickRemove);
+        remove2.setOnClickListener(onCLickRemove);
+        remove3.setOnClickListener(onCLickRemove);
+        remove4.setOnClickListener(onCLickRemove);
+        remove5.setOnClickListener(onCLickRemove);
+
+
+        TextView address = new TextView(this);
+        ImageButton remove = new ImageButton(this);
+        if (Preferences.getNumberOfAddresses("Address",this)!=0) {
+            for (int i = 0; i < Preferences.getNumberOfAddresses("Address", this); i++) {
+                address = popupView.findViewWithTag(i);
+                remove = popupView.findViewWithTag(i+5);
+                address.setVisibility(View.VISIBLE);
+                address.setText(Preferences.getPrefAddresses("Address", this).get(i));
+                remove.setVisibility(View.VISIBLE);
+            }
+        }
 
         // check if the editText is empty and if so disable add button
         enterAddress.addTextChangedListener(new TextWatcher() {
@@ -124,8 +183,19 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
                     Toast.makeText(ProfileActivity.this, "Veuillez rentrer une adresse", Toast.LENGTH_SHORT).show();
                 }
                 else {
-                    address1.setText(enterAddress.getText().toString());
-                    enterAddress.setText("");
+                    if (Preferences.getNumberOfAddresses("Address",ProfileActivity.this)>=5){
+                        Toast.makeText(ProfileActivity.this,"Vous ne pouvez pas ajouter plus de 5 adresses",Toast.LENGTH_SHORT).show();
+                    }
+                    else {
+                        int addressIndex = Preferences.getNumberOfAddresses("Address", ProfileActivity.this);
+                        Preferences.addValue("Address",addressIndex,enterAddress.getText().toString(),ProfileActivity.this);
+                        TextView newAddress = popupView.findViewWithTag(addressIndex);
+                        ImageButton newButton = popupView.findViewWithTag(addressIndex + 5);
+                        newAddress.setVisibility(View.VISIBLE);
+                        newAddress.setText(enterAddress.getText().toString());
+                        newButton.setVisibility(View.VISIBLE);
+                        enterAddress.setText("");
+                    }
                 }
             }
 
@@ -140,19 +210,18 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
     @Override
     public void onClick(View v){
         int buttonClicked = (int) v.getTag();
-        // dim background of popup
-        dim_popup.setVisibility(View.VISIBLE);
-
-        if (buttonClicked==0){
+        switch (buttonClicked) {
+            case 0:
             //buttonView=R.layout.popup_sensibility;
-        }
-        else if (buttonClicked==1){
+            case 1:
+            // dim background of popup
+            dim_popup.setVisibility(View.VISIBLE);
             // show the popup window
             popupWindow.showAtLocation(v, Gravity.CENTER, 0, 0);
-        }
-        else{
+            case 2:
             //buttonView=R.layout.popup_favorite_transportation;
         }
+
 
     }
 

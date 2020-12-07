a807301267c4183eeb89648f9d44b94a093ea77a
Jordan <savea.jordan@gmail.com>
2020-12-07 19:24:55 +0100

    Modification taille texte spinner

--- app/src/main/java/com/example/helloworld/MapActivity.java
+++ app/src/main/java/com/example/helloworld/MapActivity.java
@@ -42,9 +42,9 @@ public class MapActivity extends AppCompatActivity {
         Spinner spinner = (Spinner) findViewById(R.id.map_spinner);
 // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                R.array.map_array, android.R.layout.simple_spinner_item);
+                R.array.map_array, R.layout.custom_spinner_item);
 // Specify the layout to use when the list of choices appears
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        adapter.setDropDownViewResource(R.layout.custom_spinner_dropdown_item);
 // Apply the adapter to the spinner
         spinner.setAdapter(adapter);
 

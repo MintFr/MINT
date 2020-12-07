7dc6641ce8915e44fb9be36a9b1835c3d09437fd
Jordan <savea.jordan@gmail.com>
2020-12-07 11:03:25 +0100

    Ajout carte en fond de la page d'accueil

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -2,16 +2,39 @@ package com.example.helloworld;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import org.osmdroid.api.IMapController;
+import org.osmdroid.config.Configuration;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+
 public class MainActivity extends AppCompatActivity {
+    private final int REQUEST_PERMISSIONS_REQUEST_CODE = 1;
+    private MapView map;
+    IMapController mapController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Context context = getApplicationContext();
+        Configuration.getInstance().load(context, PreferenceManager.getDefaultSharedPreferences(getApplicationContext()));
+
+        //Map
+        map = findViewById(R.id.mapView);
+        map.setTileSource(TileSourceFactory.MAPNIK); //render
+        map.setMultiTouchControls(true);
+        GeoPoint startPoint = new GeoPoint(47.21, -1.55);
+        mapController = map.getController();
+        mapController.setZoom(15.0);
+        mapController.setCenter(startPoint);
+
 
         //Bottom Menu
         BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);

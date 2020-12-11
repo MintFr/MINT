9c4f5067f0a9cf179ece3b3f62d39eda628aa240
Maxence <maxence.fort53@gmail.com>
2020-12-11 14:48:07 +0100

    Demande d'autorisation d'accès à la position de l'utilisateur

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -1,13 +1,17 @@
 package com.example.helloworld;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.Button;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -18,8 +22,10 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.PopupWindow;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 import java.util.ArrayList;
 
 import org.osmdroid.api.IMapController;
@@ -28,12 +34,13 @@ import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private final int REQUEST_PERMISSIONS_REQUEST_CODE = 1;
     private MapView map;
     IMapController mapController;
     private EditText startPoint;
     private EditText endPoint;
+    private int POSITION_PERMISSION_CODE = 1;
 
     PopupWindow popUp;
 
@@ -55,8 +62,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mapController.setZoom(15.0);
         mapController.setCenter(startPoint);
 
-
-
         //Bottom Menu
         BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
         bottomNav.setOnNavigationItemSelectedListener(new ActivityMenuSwitcher(this));
@@ -77,6 +82,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         // the drop down list is a list view
         ListView addressListView = new ListView(this);
 
+        // add location button to the list
+        TextView localisationRequest = new TextView(this);
+        localisationRequest.setText("Ma position");
+        localisationRequest.setTextColor(getResources().getColor(R.color.colorAccent));
+        localisationRequest.setPadding(30,30,30,0);
+        addressListView.addHeaderView(localisationRequest);
+
         // add title to the list
         TextView title = new TextView(this);
         title.setText("Mes adresses favorites");
@@ -91,6 +103,24 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         // set on item selected
         addressListView.setOnItemClickListener(onItemClickListener());
 
+        //User's position
+        localisationRequest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // If the permission is already allowed, we use the user's position
+                if (ContextCompat.checkSelfPermission(MainActivity.this,
+                        Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                    startPoint.setText("Ma position");
+                    startPoint.setSelection(startPoint.length()); // set cursor at end of text
+                    popUp.dismiss();
+                }
+                // If not, we ask the permission to use his position
+                else {
+                    requestLocalisationPermission();
+                }
+            }
+        });
+
         // some other visual settings for popup window
         popupWindow.setFocusable(true);
         popupWindow.setWidth(1000);
@@ -101,6 +131,50 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         popupWindow.setContentView(addressListView);
 
         return popupWindow;
+    }
+
+    // Ask the permission to the user to use his geolocalisation
+    private void requestLocalisationPermission(){
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+            new AlertDialog.Builder(this)
+                    .setTitle("Autorisation nécessaire")
+                    .setMessage("Nous avons besoin de votre autorisation pour utiliser votre géolocalisation.")
+                    .setPositiveButton("autoriser", new DialogInterface.OnClickListener(){
+                        @Override
+                        public void onClick(DialogInterface dialog, int which){
+                            ActivityCompat.requestPermissions(MainActivity.this, new String[] {
+                                    Manifest.permission.ACCESS_FINE_LOCATION}, POSITION_PERMISSION_CODE);
+                            startPoint.setText("Ma position");
+                            startPoint.setSelection(startPoint.length()); // set cursor at end of text
+                            popUp.dismiss();
+                        }
+                    })
+                    .setNegativeButton("annuler", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    })
+                    .create().show();
+        } else {
+            ActivityCompat.requestPermissions(this, new String[] {
+                    Manifest.permission.ACCESS_FINE_LOCATION}, POSITION_PERMISSION_CODE);
+        }
+    }
+
+    // Return the answer of the localisation permission request
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == POSITION_PERMISSION_CODE) {
+            if (grantResults.length > 0 && grantResults[0] ==  PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Autorisation ACCORDÉE", Toast.LENGTH_SHORT).show();
+            }
+            else {
+                Toast.makeText(this, "Autorisation REFUSÉE", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 
     private AdapterView.OnItemClickListener onItemClickListener(){

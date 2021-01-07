a2c53f3c38ffa2996ec2451f9687150ef5c04f31
Jordan <savea.jordan@gmail.com>
2021-01-07 10:05:08 +0100

    new build url

--- app/src/main/java/com/example/helloworld/LoadingPageActivity.java
+++ app/src/main/java/com/example/helloworld/LoadingPageActivity.java
@@ -18,10 +18,12 @@ public class LoadingPageActivity extends AppCompatActivity {
         double param2 = intent.getDoubleExtra("param2",0.0);
         double param3 = intent.getDoubleExtra("param3",0.0);
         double param4 = intent.getDoubleExtra("param4",0.0);
+        int[] param5 = intent.getIntArrayExtra("param5");
+
 
         //build url
-        String url = String.format("http://ser-info-03.ec-nantes.fr:8080/itinerarytest/itinerary?pdaLat=%s&pdaLong=%s&pddLat=%s&pddLong=%s",
-                param1, param2, param3, param4);
+        String url = String.format("http://ser-info-03.ec-nantes.fr:8080/itinerarytest/itinerary?pdaLat=%s&pdaLong=%s&pddLat=%s&pddLong=%s&transport=%s,%s,%s,%s",
+                param1, param2, param3, param4,param5[0],param5[1],param5[2],param5[3]);
         //start of the async task
         AsyncItineraryCompute task = new AsyncItineraryCompute(LoadingPageActivity.this);
         task.execute(url);

0de31bfb4fd2b5a4a761831c11bd813a0b84c895
remym <remy.maige@gmail.com>
2022-01-03 12:38:49 +0100

    Unit tests + issues (dont checkout this commit)

--- app/src/test/java/com/example/mint/ItineraryUnitTest.java
+++ app/src/test/java/com/example/mint/ItineraryUnitTest.java
@@ -3,11 +3,28 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 import com.example.mint.model.Itinerary;
 
+import java.io.File;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Paths;
+
 
 public class ItineraryUnitTest {
     @Test
     public void itineraryIsCorrect() {
-        Itinerary itinerary = new Itinerary();
 
+        File file = null;
+        URL url = this.getClass().getClassLoader().getResource("jsonTest.json");
+
+        System.out.println(url.getPath());
+        try {
+            file = Paths.get(url.toURI()).toFile();
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+
+        assertNotNull(this.getClass().getClassLoader().getResource("jsonTest.json"));
+
+        Itinerary itinerary = new Itinerary();
     }
 }

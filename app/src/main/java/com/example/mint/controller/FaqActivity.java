c1a9dbd40420568a106ca6d228de5b7e60c9efc0
Mathis Barthere <mathis.barthere@gmail.com>
2022-01-03 16:21:23 +0100

    Docs faq activity

--- app/src/main/java/com/example/mint/controller/FaqActivity.java
+++ app/src/main/java/com/example/mint/controller/FaqActivity.java
@@ -40,6 +40,7 @@ public class FaqActivity extends AppCompatActivity implements View.OnClickListen
             String idAndswer = "faq_answer_" + (i + 1);
             String idQuestion = "question" + (i + 1) + "_text";
             String idButton = "question" + (i + 1) + "_button";
+            // Getting corresponding attributes
             mAnswers.add(
                     getResources().getString(
                             getResources().getIdentifier(
@@ -63,14 +64,22 @@ public class FaqActivity extends AppCompatActivity implements View.OnClickListen
             buttonI.setOnClickListener(this);
             imageButtons.add(buttonI);
 
+            // By default questions are not expanded
             areExpanded.add(false);
         }
     }
 
+    /**
+     * Method to get the index of the corresponding button clicked with the id
+     * @param buttonId: Id of the corresponding button
+     * @param imageButtons: List of all buttons
+     * @return: index of the button in the list
+     */
     public int getIndiceButton(int buttonId, ArrayList<ImageButton> imageButtons) {
         int res = 0;
         for (int i = 0; i < nbQuestions; i++) {
             ImageButton imageButtoni = imageButtons.get(i);
+            // Check if the id corresponds to the current button of the list
             if (buttonId == imageButtoni.getId()) {
                 res = i;
                 break;
@@ -81,26 +90,41 @@ public class FaqActivity extends AppCompatActivity implements View.OnClickListen
 
     @Override
     public void onClick(View v) {
+        // Getting the index of the button clicked in the list
         int buttonId = v.getId();
         int indiceButton = getIndiceButton(buttonId, this.imageButtons);
+        // Getting all attributes of the corresponding button
+        // By construction, the indexes are equals
         boolean isExpanded = areExpanded.get(indiceButton);
         ImageButton imageButton = imageButtons.get(indiceButton);
         TextView textView = textViews.get(indiceButton);
         String mQuestion = mQuestions.get(indiceButton);
         String mAnswer = mAnswers.get(indiceButton);
 
+        // When clicked, the button switches is expansion, to call it back
         isExpanded = !isExpanded;
+        // The icon of the button changes : expand more if not expanded and conversely
         imageButton.setImageResource(
                 isExpanded
                         ? R.drawable.ic_baseline_expand_less_24
-                        : R.drawable.ic_baseline_expand_more_24);
-        textView.setText(isExpanded ? mQuestion + "\n \n" + mAnswer + "\n \n" : mQuestion);
+                        : R.drawable.ic_baseline_expand_more_24
+        );
+        // If expanded we print the question and the answer. Otherwise only the question
+        textView.setText(
+                isExpanded
+                        ? mQuestion + "\n \n" + mAnswer + "\n \n"
+                        : mQuestion
+        );
 
+        // Modifying attributes of the list
         areExpanded.set(indiceButton, isExpanded);
         textViews.set(indiceButton, textView);
 
     }
 
+    /**
+     * When user wants to go back to settings activity
+     */
     public void onClickBackButton(View view) {
         Intent intent = new Intent(this, SettingsActivity.class);
         startActivity(intent);

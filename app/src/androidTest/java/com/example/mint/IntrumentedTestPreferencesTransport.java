def88f802951d6b8b472a9178278bd0f01cdd139
TinyTinaa <lucasdj31@gmail.com>
2022-01-04 15:31:41 +0100

    preferences transport unit test working

--- app/src/androidTest/java/com/example/mint/IntrumentedTestPreferencesTransport.java
+++ app/src/androidTest/java/com/example/mint/IntrumentedTestPreferencesTransport.java
@@ -3,11 +3,13 @@
 package com.example.mint;
 
 
+        import static org.junit.Assert.assertFalse;
         import static org.junit.Assert.assertTrue;
 
         import android.content.Context;
         import android.content.SharedPreferences;
         import android.util.Log;
+        import java.util.Arrays;
 
         import androidx.test.core.app.ApplicationProvider;
         import androidx.test.platform.app.InstrumentationRegistry;
@@ -23,18 +25,59 @@ package com.example.mint;
 
 public class IntrumentedTestPreferencesTransport {
     Context context =   ApplicationProvider.getApplicationContext();
+    int[] fav = {1,0,3,0};
+
     @Before
     public void setup(){
         PreferencesTransport.clearTransportation(context);
+        PreferencesTransport.addTransportation("Transportation",0,"velo",context);
         PreferencesTransport.addTransportation("Transportation",1,"voiture",context);
+        PreferencesTransport.addTransportation("Transportation",2,"pied",context);
+        PreferencesTransport.addTransportation("Transportation",3,"transport",context);
+
+        PreferencesTransport.addTransportation("Transportation_2",0,"velo",context);
+        PreferencesTransport.addTransportation("Transportation_2",1,"voiture",context);
+        PreferencesTransport.addTransportation("Transportation_2",2,"pied",context);
+        PreferencesTransport.addTransportation("Transportation_2",3,"transport",context);
+
+
     }
 
     @Test
     public void testGetTransportation(){
 
+        assertTrue( PreferencesTransport.getPrefTransportation("Transportation", context).get(0)=="velo");
         assertTrue( PreferencesTransport.getPrefTransportation("Transportation", context).get(1)=="voiture");
+        assertTrue( PreferencesTransport.getPrefTransportation("Transportation", context).get(2)=="pied");
+        assertTrue( PreferencesTransport.getPrefTransportation("Transportation", context).get(3)=="transport");
 
 
+    }
+
+    @Test
+    public void testRemoveTransportation(){
+        PreferencesTransport.removeTransportation("Transportation_2",0,context);
+        assertFalse( PreferencesTransport.getPrefTransportation("Transportation_2", context).get(0)=="velo");
+    }
+
+    @Test
+    public void testGetOptionTransportation(){
+        int[] test = {1,0,3,0};
+        PreferencesTransport.setOptionTransportation(fav,context);
+        assertTrue(Arrays.equals(test,PreferencesTransport.getOptionTransportation(context)));
+        PreferencesTransport.clearOptionTransportation(context);
+    }
+
+    @Test
+    public void testAddOptionTransportation(){
+        int[] test = {1,2,0,0};
+
+        PreferencesTransport.setOptionTransportation(fav,context);
+        PreferencesTransport.addOptionTransportation("2",2,context);
+        PreferencesTransport.addOptionTransportation("3",0,context);
+
+        assertTrue(Arrays.equals(test,PreferencesTransport.getOptionTransportation(context)));
+        PreferencesTransport.clearOptionTransportation(context);
     }
     @After
     public void setDown(){

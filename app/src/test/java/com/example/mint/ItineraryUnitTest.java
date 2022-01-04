b3188e5ab27f9db3b81fb404a24998e913e2a59e
remym <remy.maige@gmail.com>
2022-01-04 10:07:31 +0100

    Test files + lastest modifications

--- app/src/test/java/com/example/mint/ItineraryUnitTest.java
+++ app/src/test/java/com/example/mint/ItineraryUnitTest.java
@@ -1,9 +1,15 @@
 package com.example.mint;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.junit.Test;
 import static org.junit.Assert.*;
+
+import android.renderscript.ScriptGroup;
+
 import com.example.mint.model.Itinerary;
 
 import java.io.File;
+import java.io.InputStream;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.file.Paths;
@@ -16,7 +22,7 @@ public class ItineraryUnitTest {
         File file = null;
         URL url = this.getClass().getClassLoader().getResource("jsonTest.json");
 
-        System.out.println(url.getPath());
+        //System.out.println(url.getPath());
         try {
             file = Paths.get(url.toURI()).toFile();
         } catch (URISyntaxException e) {
@@ -25,6 +31,18 @@ public class ItineraryUnitTest {
 
         assertNotNull(this.getClass().getClassLoader().getResource("jsonTest.json"));
 
-        Itinerary itinerary = new Itinerary();
+        String result = "{/D:/Dev/Mint/Fullcode/MINT/app/build/intermediates/javac/debugUnitTest/classes/jsonTest.json}";
+        try {
+            System.out.println("on entre dans le try");
+            JSONObject jObject = new JSONObject(result);
+            System.out.println("on crée je JSONObject");
+            Itinerary itinerary = new Itinerary(jObject);
+            assertEquals(itinerary.getDistance(),15.3,0.001);
+            System.out.println("on passe dans le try");
+        } catch (JSONException e){
+            e.printStackTrace();
+        }
     }
 }
+
+

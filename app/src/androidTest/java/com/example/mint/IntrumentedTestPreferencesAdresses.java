dde5040c47f08278cf18b97ef4c630bd953be958
TinyTinaa <lucasdj31@gmail.com>
2022-01-04 10:40:05 +0100

    formating

---
+++ app/src/androidTest/java/com/example/mint/IntrumentedTestPreferencesAdresses.java
@@ -0,0 +1,36 @@
+package com.example.mint;
+
+
+import static org.junit.Assert.assertTrue;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.util.Log;
+
+import androidx.test.core.app.ApplicationProvider;
+
+import com.example.mint.model.PreferencesAddresses;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class IntrumentedTestPreferencesAdresses {
+    Context context =   ApplicationProvider.getApplicationContext();
+    @Before
+    public void setup(){
+        PreferencesAddresses.clearAddresses(context);
+        PreferencesAddresses.addAddress("startAddress","ecole",context);
+    }
+
+    @Test
+    public void testGetAddress(){
+
+        assertTrue(PreferencesAddresses.getAddress("startAddress",context)=="ecole");
+    }
+    @After
+    public void setDown(){
+        PreferencesAddresses.clearAddresses(context);
+    }
+
+}

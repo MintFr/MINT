dadb277d22ca89b808f9b7f1862e56baeb304970
TinyTinaa <lucasdj31@gmail.com>
2022-01-05 15:31:50 +0100

    Creation of activity_real_time_itinerary

---
+++ app/src/main/java/com/example/mint/controller/RealTimeItineraryActivity.java
@@ -0,0 +1,16 @@
+package com.example.mint.controller;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.mint.R;
+
+public class RealTimeItineraryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_real_time_itinerary);
+    }
+}

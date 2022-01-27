208135119259e8140025a204f5b000bb255f67e4
Mathis Barthere <mathis.barthere@gmail.com>
2022-01-27 10:20:34 +0100

    format code

--- app/src/main/java/com/example/mint/controller/MainActivity.java
+++ app/src/main/java/com/example/mint/controller/MainActivity.java
@@ -399,24 +399,19 @@ public class MainActivity extends AppCompatActivity implements View.OnFocusChang
         Log.d(LOG_TAG, "onStart: finished ");
 
         //pollen_alert button color
-        int pollen_alert_count = 1;
+        int pollen_alert_count = 2;
         int colorZero = Color.parseColor("#89BE89");
         int colorOne = Color.parseColor("#FF9800");
         int colorTwo = Color.parseColor("#F00020");
 
         if (pollen_alert_count==0){
-
             pollen_button.setColorFilter(colorZero);
         }
-
-            else if (pollen_alert_count==1){
-                pollen_button.setColorFilter(colorOne);
-
-            }
-            else if (pollen_alert_count==2){
-                pollen_button.setColorFilter(colorTwo);
-
-
+        else if (pollen_alert_count==1){
+            pollen_button.setColorFilter(colorOne);
+        }
+        else if (pollen_alert_count==2){
+            pollen_button.setColorFilter(colorTwo);
         }
 
     }
